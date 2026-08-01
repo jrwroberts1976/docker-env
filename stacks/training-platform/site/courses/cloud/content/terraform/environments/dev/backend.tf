d86fb98d5947f36f881b09d24a20e008219179bd
James Roberts <jrwroberts1976@gmail.com>
2026-08-01 18:30:43 +0100

    Remove generated files and temporary repository clones

--- stacks/training-platform/site/courses/cloud/content/terraform/environments/dev/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "jrwroberts-cloud-platform-tfstate-2026"
-    key          = "dev/terraform.tfstate"
-    region       = "eu-west-2"
-    use_lockfile = true
-    encrypt      = true
-  }
-}
